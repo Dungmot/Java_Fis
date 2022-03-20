52127e293e58e81df77b09afba3ee2f2df7942ae
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-20 18:30:38 +0700

    Create InputComponent.java

---
+++ topic01/practice01/InputComponent.java
@@ -0,0 +1,20 @@
+package fis.topic01.practice01;
+
+public class InputComponent {
+	private IValidation validation;
+	private String data;
+	
+	public void setData(String data) {
+		
+	}
+	public void display() {
+		
+	}
+	public void setValidation(IValidation validation) {
+		
+	}
+	private boolean validate() {
+		return false;
+	}
+
+}
