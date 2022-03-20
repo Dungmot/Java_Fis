d1614158713284dd2d445e360a71b535a2bebb17
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-20 18:30:34 +0700

    Create Form.java

---
+++ topic01/practice01/Form.java
@@ -0,0 +1,17 @@
+package fis.topic01.practice01;
+
+import java.awt.Component;
+import java.util.List;
+
+public class Form {
+	public List<Component> components;
+	
+	public void addComponent(Component component) {
+		
+	}
+
+	public boolean validateForm() {
+		return false;
+		
+	}
+}
