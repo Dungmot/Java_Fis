8a2899f031c5ea7847988459b6fe7da79efc3d92
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-20 18:30:50 +0700

    Create StringMaxLengthValidation.java

---
+++ topic01/practice01/StringMaxLengthValidation.java
@@ -0,0 +1,9 @@
+package fis.topic01.practice01;
+
+public class StringMaxLengthValidation implements IValidation {
+	private int length;
+	
+	public boolean validate(String data) {
+		return false;
+}
+}
