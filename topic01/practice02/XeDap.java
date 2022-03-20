1a80e3cb0260639224316d27732afd1a2b7b27de
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-20 18:31:03 +0700

    Create XeDap.java

---
+++ topic01/practice02/XeDap.java
@@ -0,0 +1,13 @@
+	package fis.topic01.practice02;
+
+public class XeDap extends Xe{
+
+	@Override
+	double getPrice() {
+		// TODO Auto-generated method stub
+		return Constant.PRICE.XE_DAP;
+	}
+	
+	
+	
+}
