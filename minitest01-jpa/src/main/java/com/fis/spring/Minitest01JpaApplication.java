eff10e519b80a9dac900a82e15d65bdd238d928f
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-26 14:19:13 +0700

    Create Minitest01JpaApplication.java

---
+++ minitest01-jpa/src/main/java/com/fis/spring/Minitest01JpaApplication.java
@@ -0,0 +1,13 @@
+package com.fis.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minitest01JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Minitest01JpaApplication.class, args);
+    }
+
+}
