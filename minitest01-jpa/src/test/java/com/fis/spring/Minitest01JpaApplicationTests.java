39193895c58ee968e247853d05d4617fc6eb8f0b
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-26 14:20:53 +0700

    Create Minitest01JpaApplicationTests.java
    Practice Minitest01 - Spring jpa

---
+++ minitest01-jpa/src/test/java/com/fis/spring/Minitest01JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minitest01JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
