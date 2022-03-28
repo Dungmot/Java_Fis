1ec532871f9c11b2d19507e5bb48c30b3e5dffbd
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-28 08:20:40 +0700

    Update Account.java

--- minitest01-jpa/src/main/java/com/fis/spring/entity/Account.java
+++ minitest01-jpa/src/main/java/com/fis/spring/entity/Account.java
@@ -1,22 +1,24 @@
 package com.fis.spring.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.stereotype.Component;
+import lombok.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Component
+
+@Data
+@Entity
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountId;
     private String accountNumber;
     private String accountName;
     private Double balance;
     private Integer status;
+
 }
 
 //nơi lưu trữ các đối tượng Account
