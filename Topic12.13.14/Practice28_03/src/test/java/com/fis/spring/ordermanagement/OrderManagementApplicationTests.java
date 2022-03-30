30ad70f4bcca3beada5c099583aeb68296ba96a4
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-03-30 07:40:25 +0700

    Create OrderManagementApplicationTests.java

---
+++ Topic12.13.14/Practice28_03/src/test/java/com/fis/spring/ordermanagement/OrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.spring.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
