db6fc2f96923d83a14b885d9784c987e556a4703
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-04-02 07:44:37 +0700

    Create CustomerRepository.java

---
+++ Topic12.13.14/OrderManagementApp/src/main/java/com/fis/spring/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.fis.spring.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+}
