48a86680811e32d88df723dce241297931dcdf9a
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-04-05 01:02:54 +0700

    Create CriminalEvidenceManagementSystemApplicationTests.java

---
+++ Final_test_APhuoc/src/test/java/com/fis/finaltest/CriminalEvidenceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.finaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalEvidenceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
