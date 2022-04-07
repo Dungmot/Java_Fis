c63a39243940322a2ace310e5e5dfd1d7844feba
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-04-07 18:19:41 +0700

    Delete AccountRepo.java

--- Final_test/src/main/java/com/fis/spring/repo/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.fis.spring.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.fis.spring.entity.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Long>{
-}
