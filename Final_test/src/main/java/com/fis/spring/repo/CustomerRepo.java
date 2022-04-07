38d8fc5775aea20f8e3ae79749355e2bc6a10977
Dung Hoai <88993743+Dungmot@users.noreply.github.com>
2022-04-07 18:19:44 +0700

    Delete CustomerRepo.java

--- Final_test/src/main/java/com/fis/spring/repo/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.fis.spring.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fis.spring.entity.Customer;
-
-public interface CustomerRepo extends JpaRepository<Customer, Long>{
-
-}
